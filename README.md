a1a8fdc2943a0247063d56865dfbd65a1b2754cb
Jules <juliaghany@msn.com>
2023-02-23 09:57:53 -0600

    start creating some functions in js file

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 -
 <!-- Module 5 Activity 25 for reference for displaying current day -->
 <!-- https://day.js.org/docs/en/parse/now -->
+<!-- https://day.js.org/docs/en/get-set/hour -->
--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-var saveBtn = $(".saveBtn"); 
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -11,22 +10,50 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+
+  $(".saveBtn").on("click", fucntion() {
+    var time =
+    var userInput = 
+  }
+  )
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
-  
-  //
+  // the current hour would be whatever time-block the user is putting their plan in right?
+
+  var hour = dayjs().hour()
+  $(".time-block").each
+
+  if (hour > currentHour) {
+    addClass.("past");
+  }
+
+  else if (hour === currentHour) {
+    addClass.("present");
+  }
+
+  else (hour < currentHour) {
+    addCLass.("future");
+  }
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
+
+
   // TODO: Add code to display the current date in the header of the page.
   var currentDay = dayjs();
   $("#currentDay").text(currentDay.format("MMM D, YYYY"))
 });
 
 
+// localStorage.setItem sets what is stored
+// localStorage.getItem gets what is stored
