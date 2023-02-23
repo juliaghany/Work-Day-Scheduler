38351b1cdfef59af48533381b056c6d552508c78
Jules <juliaghany@msn.com>
2023-02-23 09:17:28 -0600

    change some id selectors in html

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
